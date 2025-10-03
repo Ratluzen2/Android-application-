989dbdc70a368254098052fca2c017fadc6bc01a
Ratluzen Services <zafer.s.2.693@gmail.com>
2025-10-03 21:38:31 +0300

    Create DeviceId.kt

---
+++ app/src/main/java/com/zafer/smm/util/DeviceId.kt
@@ -0,0 +1,11 @@
+package com.zafer.smm.util
+
+import android.content.Context
+import android.provider.Settings
+
+object DeviceIdProvider {
+    fun getAndroidId(ctx: Context): String {
+        return Settings.Secure.getString(ctx.contentResolver, Settings.Secure.ANDROID_ID)
+            ?: "unknown-device"
+    }
+}
